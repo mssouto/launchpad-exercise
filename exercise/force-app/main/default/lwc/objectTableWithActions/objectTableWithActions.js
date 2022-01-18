ad16276d09fbe8059d3ad3aeb55035ee6c2afa60
Sofia <mssouto@icloud.com>
2022-01-18 02:31:07 -0300

    custom datatable working for cases

---
+++ exercise/force-app/main/default/lwc/objectTableWithActions/objectTableWithActions.js
@@ -0,0 +1,78 @@
+import { LightningElement,api, track } from 'lwc';
+import fetchData from '@salesforce/apex/ObjectTableWithActionsHelper.fetchData';
+import followRecord from '@salesforce/apex/ObjectTableWithActionsHelper.followRecord';
+import unfollowRecord from '@salesforce/apex/ObjectTableWithActionsHelper.unfollowRecord';
+
+const actions = [
+    { label: 'Follow', name: 'follow' },
+    { label: 'Unfollow', name: 'unfollow' },
+];
+
+
+export default class ObjectTableWithActions extends LightningElement {
+    @track data = [];
+    @track columnsInfo = [];
+
+    @api columns;
+    @api objectName;
+    @api selectFields;
+    @api whereCondition;
+
+    async connectedCallback() {
+
+        console.log("paso aca");
+        
+        this.data = await fetchData({   objectName: this.objectName,
+                                        selectFields: this.selectFields,
+                                        whereCondition: this.whereCondition
+                                    });
+        if (this.objectName == 'Case') {
+            this.data.forEach(item => {
+                let priority = item.Priority + "";
+                switch (priority) {
+                    case "High":
+                        item.format =  'slds-icon-standard-quip';
+                        break;
+                    case "Medium":
+                        item.format = 'slds-icon-standard-case';
+                        break;
+                    default:
+                        item.format = 'slds-icon-standard-approval';
+
+                }
+            });
+        }
+        this.columnsInfo = [
+            ...this.columnsInfo, 
+            ...JSON.parse(this.columns), 
+            ... [{
+                    type: 'action',
+                    typeAttributes: { rowActions: actions },
+                }]];
+        this.columnsInfo.forEach(item => {
+            item.cellAttributes = {
+                class: {
+                    fieldName: `format`
+                }
+            };
+        });
+                
+        
+    }
+
+    handleRowAction(event) {
+        console.log("data", event.detail);
+        const actionName = event.detail.action.name;
+        //const recordId = event.detail.row.Id;
+        switch (actionName) {
+            case 'follow':
+                followRecord({recordId : event.detail.row.Id});
+                break;
+            case 'unfollow':
+                unfollowRecord({recordId : event.detail.row.Id});
+                break;
+            default:
+        }
+    }
+
+}
